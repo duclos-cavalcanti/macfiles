9f5b9894deb5cfadeba9c4d868a7b70d0ca95628
Daniel Duclos-Cavalcanti <duclos@MacBook-Pro.lan>
2025-09-27 22:44:17 -0400

    Update

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+# installing zsh plugin
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
   echo "Installing Oh My Zsh..."
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
+
+# installing rust
+curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
