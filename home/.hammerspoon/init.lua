6c231bc7bed7332745d209cfa80728e9af9f884a
Daniel Duclos-Cavalcanti <daniel.duclos.cavalcanti@gmail.com>
2025-10-09 22:28:39 -0400

    Update

--- home/.hammerspoon/init.lua
+++ home/.hammerspoon/init.lua
@@ -4,17 +4,32 @@ function notification(text)
 end
 
 function focusOrLaunch(app_name)
-  local app = hs.application.get(app_name)
-  if app then
-    app:activate()
-  else
-    hs.application.launchOrFocus(app_name)
+    local app = hs.application.get(app_name)
+    if app then
+        app:activate()
+    else
+        hs.application.launchOrFocus(app_name)
   end
 end
 
--- FocusOrLaunch Terminal
+-- Terminal: Focus or Launch
 hs.hotkey.bind({'cmd', 'shift'}, '0', function()
     focusOrLaunch("Terminal")
+end)
+
+-- Safari: Move to the next tab group
+hs.hotkey.bind({"cmd", "ctrl"}, "l", function()
+    local app = hs.application.frontmostApplication()
+    if app:title() == "Safari" then
+        app:selectMenuItem({"Window", "Go to Next Tab Group"})
+    end
+end)
+
+hs.hotkey.bind({"cmd", "ctrl"}, "h", function()
+    local app = hs.application.frontmostApplication()
+    if app:title() == "Safari" then
+        app:selectMenuItem({"Window", "Go to Previous Tab Group"})
+    end
 end)
 
 -- Use screencapture to copy a screenshot to the clipboard
