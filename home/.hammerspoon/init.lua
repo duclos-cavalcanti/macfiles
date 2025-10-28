10878245f8b07be2c8a6ee5a6033ec21eed56f1b
Daniel Duclos-Cavalcanti <dduclos-cavalcanti@ripple.com>
2025-10-28 11:51:07 -0400

    Update

--- home/.hammerspoon/init.lua
+++ home/.hammerspoon/init.lua
@@ -12,9 +12,28 @@ function focusOrLaunch(app_name)
   end
 end
 
--- Use screencapture to copy a screenshot to the clipboard
-hs.hotkey.bind({'cmd', 'shift'}, 'C', function()
-  hs.task.new("/usr/sbin/screencapture", nil, {"-i", "-c"}):start()
+hs.hotkey.bind({"cmd", "shift"}, "1", function()
+    local currentWindow = hs.window.focusedWindow()
+    if not currentWindow then return end
+    hs.spaces.moveWindowToSpace(currentWindow, 1)
+end)
+
+hs.hotkey.bind({"cmd", "shift"}, "2", function()
+    local currentWindow = hs.window.focusedWindow()
+    if not currentWindow then return end
+    hs.spaces.moveWindowToSpace(currentWindow, 2)
+end)
+
+hs.hotkey.bind({"cmd", "shift"}, "3", function()
+    local currentWindow = hs.window.focusedWindow()
+    if not currentWindow then return end
+    hs.spaces.moveWindowToSpace(currentWindow, 3)
+end)
+
+hs.hotkey.bind({"cmd", "shift"}, "4", function()
+    local currentWindow = hs.window.focusedWindow()
+    if not currentWindow then return end
+    hs.spaces.moveWindowToSpace(currentWindow, 4)
 end)
 
 -- Safari: Move to the next tab group
@@ -31,7 +50,7 @@ hs.hotkey.bind({"cmd", "ctrl"}, "j", function()
     app:selectMenuItem({"Window", "Go to Previous Tab Group"})
 end)
 
-hs.hotkey.bind({"cmd", "ctrl"}, "o", function()
+hs.hotkey.bind({"cmd", "ctrl"}, "p", function()
     local screens = hs.screen.allScreens()
     if #screens <= 1 then 
         return
@@ -51,7 +70,7 @@ hs.hotkey.bind({"cmd", "ctrl"}, "o", function()
     local nextScreen = screens[nextIndex]
     local nextScreenFrame = nextScreen:frame()
 
-    hs.mouse.setAbsolutePosition(hs.geometry.rectMidPoint(nextScreenFrame))
+    hs.mouse.absolutePosition(hs.geometry.rectMidPoint(nextScreenFrame))
 
     local windows = hs.window.orderedWindows()
     for _, window in ipairs(windows) do
@@ -60,4 +79,32 @@ hs.hotkey.bind({"cmd", "ctrl"}, "o", function()
             break
         end
     end
+end)
+
+hs.hotkey.bind({"cmd", "shift"}, "p", function()
+    local screens = hs.screen.allScreens()
+    if #screens <= 1 then return end
+
+    local currentWindow = hs.window.focusedWindow()
+    if not currentWindow then return end
+
+    local currentScreen = currentWindow:screen()
+    local currentIndex = 1
+
+    for i, screen in ipairs(screens) do
+        if screen:id() == currentScreen:id() then
+            currentIndex = i
+            break
+        end
+    end
+
+    local nextIndex = (currentIndex % #screens) + 1
+    local nextScreen = screens[nextIndex]
+
+    currentWindow:moveToScreen(nextScreen)
+end)
+
+-- Use screencapture to copy a screenshot to the clipboard
+hs.hotkey.bind({'cmd', 'shift'}, 'C', function()
+    hs.task.new("/usr/sbin/screencapture", nil, {"-i", "-c"}):start()
 end)
