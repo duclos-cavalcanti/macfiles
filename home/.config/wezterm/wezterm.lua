4cef168b53f4c91baaebb996ef2386933d1d87b3
Daniel Duclos-Cavalcanti <daniel.duclos.cavalcanti@gmail.com>
2025-11-04 09:01:11 -0500

    Update

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -1,6 +1,9 @@
 local wezterm = require("wezterm")
-local config = wezterm.config_builder()
 local theme = require("theme")
+local keys = require("keys")
+
+
+local config = wezterm.config_builder()
 
 -- theme
 config = theme.setup(config)
@@ -16,8 +19,7 @@ config.tab_bar_at_bottom = true
 config.use_fancy_tab_bar = false
 
 -- window
--- config.window_decorations = "TITLE | RESIZE"
-config.window_decorations = "RESIZE"
+config.window_decorations = "RESIZE" -- "TITLE | RESIZE"
 config.window_background_opacity = 1.0
 config.macos_window_background_blur = 10
 config.window_padding = {
@@ -32,45 +34,7 @@ config.scrollback_lines = 3500
 
 -- keys
 if false then 
-    config.leader = { key = 'b', mods = 'CTRL', timeout_milliseconds = 1000 }
-    config.keys = {
-      { key = 'v', mods = 'LEADER', action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' } },
-      { key = 's', mods = 'LEADER', action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' } },
-      { key = 'x', mods = 'LEADER', action = wezterm.action.CloseCurrentPane { confirm = true } },
-      { key = 'X', mods = 'LEADER|SHIFT', action = wezterm.action.CloseCurrentTab { confirm = true } },
-      { key = 'c', mods = 'LEADER', action = wezterm.action.SpawnTab 'CurrentPaneDomain' },
-      { key = 'y', mods = 'LEADER', action = wezterm.action.ActivateCopyMode },
-      { key = 'Y', mods = 'LEADER|SHIFT', action = wezterm.action.Search 'CurrentSelectionOrEmptyString' },
-      { key = 'p', mods = 'LEADER', action = wezterm.action.PasteFrom 'Clipboard' },
-      { key = 'h', mods = 'LEADER', action = wezterm.action.ActivatePaneDirection 'Left' },
-      { key = 'j', mods = 'LEADER', action = wezterm.action.ActivatePaneDirection 'Down' },
-      { key = 'k', mods = 'LEADER', action = wezterm.action.ActivatePaneDirection 'Up' },
-      { key = 'l', mods = 'LEADER', action = wezterm.action.ActivatePaneDirection 'Right' },
-      { key = 'UpArrow', mods = 'CTRL|SHIFT', action = wezterm.action.MoveTabRelative(-1) },
-      { key = 'DownArrow', mods = 'CTRL|SHIFT', action = wezterm.action.MoveTabRelative(1) },
-      { key = 'w', mods = 'LEADER', action = wezterm.action.ShowLauncherArgs { flags = 'WORKSPACES' } },
-      { key = '$', mods = 'LEADER|SHIFT', action = wezterm.action.PromptInputLine {
-        description = 'Enter new workspace name',
-        action = wezterm.action_callback(function(window, pane, line)
-          if line then
-            wezterm.mux.rename_workspace(wezterm.mux.get_active_workspace(), line)
-          end
-        end),
-      }},
-      { key = ',', mods = 'LEADER', action = wezterm.action.PromptInputLine {
-        description = 'Enter new tab name',
-        action = wezterm.action_callback(function(window, pane, line)
-          if line then
-            window:active_tab():set_title(line)
-          end
-        end),
-      }},
-    }
-    
-    config.key_tables = {
-      copy_mode = wezterm.gui.default_key_tables().copy_mode,
-      search_mode = wezterm.gui.default_key_tables().search_mode,
-    }
+    config = keys.setup(config)
 end
 
 return config
