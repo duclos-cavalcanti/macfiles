7c711c2e3e4dd200939b4b97c0800121e4e122e2
Daniel Duclos-Cavalcanti <dduclos-cavalcanti@ripple.com>
2025-12-11 09:11:59 -0800

    Update

--- home/.hammerspoon/config/ctrl.lua
+++ home/.hammerspoon/config/ctrl.lua
@@ -2,8 +2,36 @@ local M = {}
 
 function M.fillWindow()
     local app = hs.application.frontmostApplication()
-    if app then 
+    if app then
         app:selectMenuItem({"Window", "Fill"})
+    end
+end
+
+function M.MoveWindowLeft()
+    local app = hs.application.frontmostApplication()
+    if app then
+        app:selectMenuItem({"Window", "Move & Resize", "Left"})
+    end
+end
+
+function M.MoveWindowRight()
+    local app = hs.application.frontmostApplication()
+    if app then
+        app:selectMenuItem({"Window", "Move & Resize", "Right"})
+    end
+end
+
+function M.MoveWindowTop()
+    local app = hs.application.frontmostApplication()
+    if app then
+        app:selectMenuItem({"Window", "Move & Resize", "Top"})
+    end
+end
+
+function M.MoveWindowBottom()
+    local app = hs.application.frontmostApplication()
+    if app then
+        app:selectMenuItem({"Window", "Move & Resize", "Bottom"})
     end
 end
 
