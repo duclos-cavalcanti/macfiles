0ba7a804d115b419d0209ce002680c0c97230e07
Daniel Duclos-Cavalcanti <dduclos-cavalcanti@ripple.com>
2025-12-15 15:05:03 -0300

    Update

--- home/.hammerspoon/init.lua
+++ home/.hammerspoon/init.lua
@@ -8,13 +8,18 @@ local bind   = utils.bind
 local task   = utils.task
 local lazy   = utils.lazy
 
-bind({"cmd", "shift"},    "p",    screen.FocusNextScreen)
-bind({"cmd", "ctrl"},     "p",    screen.MoveToNextScreen)
-bind({"cmd", "shift"},    "f",    ctrl.fillWindow)
-bind({"cmd", "shift"},    "h",    ctrl.MoveWindowLeft)
-bind({"cmd", "shift"},    "k",    ctrl.MoveWindowTop)
-bind({"cmd", "shift"},    "j",    ctrl.MoveWindowBottom)
-bind({"cmd", "shift"},    "l",    ctrl.MoveWindowRight)
-bind({"cmd", "ctrl"},     "h",    apps.SafariMoveToPreviousTabGroup)
-bind({"cmd", "ctrl"},     "l",    apps.SafariMoveToNextTabGroup)
-bind({"cmd", "shift"},    "c",    lazy(task, "/usr/sbin/screencapture", {"-i", "-c"}))
+local terminal="Wezterm"
+local browser="Safari"
+
+bind({"cmd", "shift"},      "p",    screen.FocusNextScreen)
+bind({"cmd", "ctrl"},       "p",    screen.MoveToNextScreen)
+bind({"cmd", "shift"},      "f",    ctrl.fillWindow)
+bind({"cmd", "shift"},      "h",    ctrl.MoveWindowLeft)
+bind({"cmd", "shift"},      "k",    ctrl.MoveWindowTop)
+bind({"cmd", "shift"},      "j",    ctrl.MoveWindowBottom)
+bind({"cmd", "shift"},      "l",    ctrl.MoveWindowRight)
+bind({"cmd", "ctrl"},       "h",    apps.SafariMoveToPreviousTabGroup)
+bind({"cmd", "ctrl"},       "l",    apps.SafariMoveToNextTabGroup)
+bind({"cmd", "shift"},      "c",    lazy(task, "/usr/sbin/screencapture", {"-i", "-c"}))
+bind({"cmd"},               "0",    lazy(apps.focusOrLaunchApp, terminal))
+bind({"cmd"},               "1",    lazy(apps.focusOrLaunchApp, browser))
