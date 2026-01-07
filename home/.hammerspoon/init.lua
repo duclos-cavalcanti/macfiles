a288bfb01bd7127d157c6a640bec19ee922f995c
Daniel Duclos-Cavalcanti <dduclos-cavalcanti@ripple.com>
2026-01-07 18:23:41 -0300

    Update

--- home/.hammerspoon/init.lua
+++ home/.hammerspoon/init.lua
@@ -10,11 +10,9 @@ local lazy   = utils.lazy
 local terminal="WezTerm"
 local browser="Safari"
 
+bind({"cmd"},               "0",        lazy(apps.focusBetweenApps, terminal, browser))
+bind({"cmd", "shift"},      "c",        lazy(task, "/usr/sbin/screencapture", {"-i", "-c"}))
 bind({"cmd", "shift"},      "p",        screen.FocusNextScreen)
 bind({"cmd", "ctrl"},       "p",        screen.MoveToNextScreen)
-
 bind({"cmd", "ctrl"},       "h",        apps.SafariMoveToPreviousTabGroup)
 bind({"cmd", "ctrl"},       "l",        apps.SafariMoveToNextTabGroup)
-
-bind({"cmd", "shift"},      "c",        lazy(task, "/usr/sbin/screencapture", {"-i", "-c"}))
-bind({"cmd"},               "0",        lazy(apps.focusBetweenApps, terminal, browser))
