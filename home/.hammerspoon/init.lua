f3eea7412d824b2ccaaffc7daad35d0b8dae06c4
Daniel Duclos-Cavalcanti <dduclos-cavalcanti@ripple.com>
2026-02-19 10:55:51 -0500

    Update

--- home/.hammerspoon/init.lua
+++ home/.hammerspoon/init.lua
@@ -15,5 +15,10 @@ bind({"cmd", "ctrl"},       "p",        screen.MoveToNextScreen)
 
 bind({"cmd", "ctrl"},       "f",        lazy(apps.executeOnApp, function(app) app:selectMenuItem({"Window", "Fill"}) end))
 
-bind({"cmd", "ctrl"},       "h",        lazy(apps.executeOnTitle, "Safari", function(app) app:selectMenuItem({"Window", "Go to Previous Tab Group"}) end))
-bind({"cmd", "ctrl"},       "l",        lazy(apps.executeOnTitle, "Safari", function(app) app:selectMenuItem({"Window", "Go to Next Tab Group"}) end))
+bind({"cmd", "ctrl"},       "h",        lazy(apps.executeOnApp, function(app) app:selectMenuItem({"Window", "Move & Resize", "Left"}) end))
+bind({"cmd", "ctrl"},       "j",        lazy(apps.executeOnApp, function(app) app:selectMenuItem({"Window", "Move & Resize", "Bottom"}) end))
+bind({"cmd", "ctrl"},       "k",        lazy(apps.executeOnApp, function(app) app:selectMenuItem({"Window", "Move & Resize", "Top"}) end))
+bind({"cmd", "ctrl"},       "l",        lazy(apps.executeOnApp, function(app) app:selectMenuItem({"Window", "Move & Resize", "Right"}) end))
+
+bind({"cmd", "ctrl"},       "[",        lazy(apps.executeOnTitle, "Safari", function(app) app:selectMenuItem({"Window", "Go to Previous Tab Group"}) end))
+bind({"cmd", "ctrl"},       "]",        lazy(apps.executeOnTitle, "Safari", function(app) app:selectMenuItem({"Window", "Go to Next Tab Group"}) end))
