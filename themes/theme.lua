0357b3a308da7e5aaac9a5a9a7ba0847435fbc07
Daniel Duclos-Cavalcanti <daniel.duclos.cavalcanti@gmail.com>
2026-02-22 11:21:03 -0500

    Update

--- themes/theme.lua
+++ themes/theme.lua
@@ -1,6 +1,6 @@
 local theme = "gruvbox"
 
-local path = "/Users/dduclos-cavalcanti/.macfiles/themes/" .. theme .. ".lua"
+local path = os.getenv("MACFILES") .. "/themes/" .. theme .. ".lua"
 
 local M = dofile(path)
 return M
