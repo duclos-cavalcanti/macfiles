9f41db99c5b023aaba13123f4e061591a4845847
Daniel Duclos-Cavalcanti <dduclos-cavalcanti@ripple.com>
2026-03-10 09:34:48 -0400

    Update

--- home/.config/wezterm/config/init.lua
+++
@@ -1,9 +0,0 @@
-local M = {
-    workspace   = require("config.workspace"),
-    tab         = require("config.tab"),
-    theme       = require("config.theme"),
-    utils       = require("config.utils")
-}
-
-
-return M
