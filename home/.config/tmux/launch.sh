eb95bb2daef02b31a2676d5ea991d241b60219fd
Daniel Duclos-Cavalcanti <dduclos-cavalcanti@ripple.com>
2026-03-10 09:47:40 -0400

    Update

--- home/.config/tmux/launch.sh
+++ home/.config/tmux/launch.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 main() {
+    cd "$HOME"/Work
     local folder=$(fd --follow --hidden --type d \
         --exclude .git \
         --exclude .cache \
@@ -8,7 +9,9 @@ main() {
         --exclude node_modules \
         --exclude target \
         --exclude dist \
-        . "$HOME" \
+        -d 3 \
+        .  \
+        | sort \
         | fzf --layout=reverse --tmux center,60%,border-native --exit-0 --prompt="launch > ")
 
     [[ -z "$folder" ]] && exit 0
