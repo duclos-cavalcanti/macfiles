a76589f9c488ff9dba74d7604247a81c8b57d39e
Daniel Duclos-Cavalcanti <dduclos-cavalcanti@ripple.com>
2026-03-10 11:38:48 -0400

    Update

--- home/.config/tmux/sessions.sh
+++ home/.config/tmux/sessions.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 main() {
-    local session=$(tmux list-sessions -F '#{session_name}' | fzf --layout=reverse --tmux center,40%,border-native --exit-0)
+    local session
+    local sessions
+    local max_len
+
+    sessions=$(tmux list-sessions -F '#{session_name}|#{session_path}')
+    max_len=$(echo "$sessions" | awk -F'|' '{print length($1)}' | sort -n | tail -1)
+    session=$(echo "$sessions" | awk -F'|' -v max="$max_len" '{printf "%s|%-*s  %s\n", $1, max, $1, $2}' | \
+        fzf --layout=reverse --tmux center,60%,border-native --exit-0 --delimiter='|' --with-nth=2 | \ 
+        cut -d'|' -f1)
 
     if [[ -n "$session" ]]; then
-      tmux switch-client -t "$session"
+        tmux switch-client -t "$session"
     fi
 }
 
