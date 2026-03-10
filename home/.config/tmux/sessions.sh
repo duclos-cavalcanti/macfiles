5d4990404f41202bb79abc4eaa6c385aaa731104
Daniel Duclos-Cavalcanti <dduclos-cavalcanti@ripple.com>
2026-03-10 09:26:58 -0400

    Update

--- home/.config/tmux/sessions.sh
+++ home/.config/tmux/sessions.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 main() {
-    # local session=$(tmux list-sessions -F '#{session_name}|#{session_name}: #{session_path}' | \
-    #     fzf --layout=reverse --tmux center,60%,border-native --exit-0 --delimiter='|' --with-nth=2 | \
-    #     cut -d'|' -f1)
     local session=$(tmux list-sessions -F '#{session_name}' | fzf --layout=reverse --tmux center,40%,border-native --exit-0)
 
     if [[ -n "$session" ]]; then
