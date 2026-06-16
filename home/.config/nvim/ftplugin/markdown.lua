bad3dfc6cea5a9dcd08c8ac66eb479ee40bb17ae
Daniel Duclos-Cavalcanti <dduclos-cavalcanti@ripple.com>
2026-06-15 20:06:18 -0400

    Update

--- home/.config/nvim/ftplugin/markdown.lua
+++ home/.config/nvim/ftplugin/markdown.lua
@@ -7,3 +7,7 @@ vim.cmd [[setlocal tabstop=2 ]]
 vim.cmd [[setlocal softtabstop=2]]
 vim.cmd [[setlocal shiftwidth=2]]
 vim.cmd [[setlocal nowrap]]
+-- Hard-wrap at 80: insert a real newline once a line crosses the threshold.
+vim.opt_local.textwidth = 80
+vim.opt_local.formatoptions:append("t") -- auto-wrap text using textwidth
+vim.opt_local.colorcolumn = "80"        -- visual guide at the limit
